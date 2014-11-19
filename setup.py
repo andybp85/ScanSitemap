281ea862bf850d16bc3b4ac4a32b0c6f409e5287
Andrew Stanish <andy@youshallthrive.com>
2014-11-19 15:57:24 -0500

    fixing dir structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 19, 2014
+
+@author: andrew
+'''
+from setuptools import setup
+
+setup(name='ScanSitemap',
+      version='1.0',
+      description="Scan all links on a page's sitemap",
+      url='https://github.com/andybp85/ScanSitemap',
+      author='Andrew Stanish',
+      author_email='andybp85@gmail.com',
+      license='MIT',
+      packages=['ScanSitemap'],
+      zip_safe=False)
